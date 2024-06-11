fec5d9dbbe674684ef0ae038bb5048cdf1e91845
whjin <wuhuajin09@163.com>
2024-06-11 22:16:46 +0800

    提交更新main主分支

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 提交main主分支
+git add .
+git commit -m "提交更新main主分支"
+git push origin main
+
+# 提交dist至gh-pages分支
+cd dist 
+git add .
+git commit -m "提交部署gh-pages分支"
+
+# 按任意键关闭
+echo "按任意键关闭"
+
+#关闭sh脚本
+exit 0
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import VueDevTools from 'vite-plugin-vue-devtools';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/vue3-typescript/' : './',
+    plugins: [
+      vue(),
+      vueJsx(),
+      VueDevTools(),
+      AutoImport({ resolvers: [ElementPlusResolver()] }),
+      Components({ resolvers: [ElementPlusResolver()] })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  };
+});
