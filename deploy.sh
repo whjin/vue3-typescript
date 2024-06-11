9bad4ace341b921bb2e0fbe787eaa2fee375f253
whjin <wuhuajin09@163.com>
2024-06-11 23:31:52 +0800

    提交更新main主分支

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# 构建 && 部署
+# 构建
 npm run build
 
 # 提交main主分支
@@ -8,10 +8,8 @@ git add .
 git commit -m "提交更新main主分支"
 git push origin main
 
-# 提交gh-pages分支
-git -f add dist
-git commit -m "提交部署gh-pages分支"
-git push origin gh-pages
+# 部署
+npm run deploy
 
 # 按任意键关闭
 echo "按任意键关闭"
