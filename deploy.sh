43ae8094b6a842fa9e7c733db5a8f551fe3353bc
whjin <wuhuajin09@163.com>
2024-06-11 22:21:47 +0800

    提交更新main主分支

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,18 @@ git add .
 git commit -m "提交更新main主分支"
 git push origin main
 
-# 提交dist至gh-pages分支
-cd dist 
+# 进入dist文件夹
+cd dist
+
+# 提交gh-pages分支
 git add .
 git commit -m "提交部署gh-pages分支"
+git push origin gh-pages
 
 # 按任意键关闭
 echo "按任意键关闭"
 read -n 1
 
-#关闭sh脚本
+#关闭脚本
 exit 0
 
